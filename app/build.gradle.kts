29a5a18cb6f3ea1fd5d31a0fd0ab15a90712f6f2
li3osoma <liza031008@mail.ru>
2023-11-15 00:36:46 +0300

    33 -> 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.musicapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.musicapplication"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
