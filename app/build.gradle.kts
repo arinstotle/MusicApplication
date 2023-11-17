33ff51731018000a9020a8284317ae757f0869fb
li3osoma <liza031008@mail.ru>
2023-11-17 23:52:29 +0300

    empty commit (only retrofit dependencies)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-viewbinding")
 
     implementation("io.coil-kt:coil-compose:2.0.0-rc01")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
